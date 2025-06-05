6df07ba2dbc12f2863dab14c352d44cbfbb2a242
sandesh4747 <sangitthapa328@gmail.com>
2025-06-05 10:31:38 +0545

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import path from "path";
-
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
-
+import path from "path";
 import { connectDB } from "./lib/db.js";
+const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-const __dirname = path.resolve();
-
-app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
+app.use(express.json({ limit: "10mb" })); // allows you to parse the body of request
 app.use(cookieParser());
+
+//ROUTES
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
@@ -37,7 +35,8 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(PORT, () => {
-  console.log("Server is running on http://localhost:" + PORT);
+app.listen(5000, () => {
+  console.log(`Server is running on http://localhost:${PORT} `);
+
   connectDB();
 });
